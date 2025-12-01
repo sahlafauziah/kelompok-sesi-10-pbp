f88ed559f61d9a2362da429d94e4219e558e209c
sahlafauziah <sahlanurulfauziah18@gmail.com>
2025-12-01 18:32:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -17,3 +17,34 @@ fetch(`http://localhost:3000/mahasiswa/${mahasiswaNim}`, {
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
 
+
+const mahasiswanim = '202403'; 
+
+fetch(`http://localhost:3000/mahasiswa/${mahasiswanim}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(data => console.log('DELETE Response:', data))
+    .catch(error => console.error('DELETE Error:', error));
+
+    const newData = {
+    nim: '202430',
+    nama: 'Dewi Purnama',
+    gender: 'P',
+    alamat: 'Cisaat',
+    prodi: 'TI'
+};
+
+fetch('http://localhost:3000/mahasiswa', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newData)
+})
+    .then(response => response.json())
+    .then(data => console.log('POST Response:', data))
+    .catch(error => console.error('POST Error:', error));
